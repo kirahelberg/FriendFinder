dc2356d69073efa0884463e92a3230e2d1e0d8d0
Kira Helberg <kira.helberg@gmail.com>
2019-04-14 19:14:26 -0400

    Updated survey submit form

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,8 @@ const friendsData = [
     name: "Ahmed",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+    totalScore: 5 + 1 + 4 + 4 + 5 + 1 + 2 + 5 + 4 + 1
   }
 ];
 
